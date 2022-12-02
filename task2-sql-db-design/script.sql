c842036984a4702713e205fa388a873b504da9c0
mfnsvrtm <74641121+mfnsvrtm@users.noreply.github.com>
2022-12-02 13:15:12 +0400

    adjust length limits on `VARCHAR` columns
    - from 30 to 100 for `purchase.bank_account_number`
    - from 30 to 100 for `user.email`

--- task2-sql-db-design/script.sql
+++ task2-sql-db-design/script.sql
@@ -29,7 +29,7 @@ CREATE TABLE purchase (
     bid_id SERIAL NOT NULL,
     amount DECIMAL(9, 2) NOT NULL,
     payment_method VARCHAR(30) NOT NULL,
-    bank_account_number VARCHAR(30) NOT NULL
+    bank_account_number VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE "user" (
@@ -38,7 +38,7 @@ CREATE TABLE "user" (
     password VARCHAR(30) NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     phone_number VARCHAR(30) NOT NULL,
     address_id SERIAL NOT NULL
 );
